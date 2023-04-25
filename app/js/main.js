a3b5f950cac5d1d2f2bf737a7aa68256de6e4a67
GolovanovAlex <Golovanov.a.a@yandex.ru>
2023-04-25 10:54:51 +0300

    slider +

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,7 @@
-// ---- ---- menu button ---- ---- //
-const menuBtn = document.querySelector('.menu__btn');
-const menuLink = document.querySelector('.menu');
-
-menuBtn.addEventListener('click', () => {
-  menuBtn.classList.toggle('menu__btn--active');
-  menuLink.classList.toggle('menu--active');
+// ---- ---- swiper ---- ---- //
+const swiper = new Swiper('.mySwiper', {
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 });
-// ---- ---- scroll  ---- ---- //
-const anchors = document.querySelectorAll('a[href*="#"]');
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-
-    const blockID = anchor.getAttribute('href').substr(1);
-
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-  });
-}
